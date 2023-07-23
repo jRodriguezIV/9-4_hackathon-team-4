a7149669e51458988f5d1527df55006f264c70d9
arerimr <raydelys.morrobelr@gmail.com>
2023-07-23 14:02:06 -0400

    splash page details

--- src/Pages/SplashPage/SplashPage.js
+++ src/Pages/SplashPage/SplashPage.js
@@ -5,6 +5,9 @@ import scrambled from '../../assets/scrambled.png'
 import solvedPic from '../../assets/solved.png'
 import './SplashPage.css'
 import { Link } from 'react-router-dom'
+import fav from '../../assets/blueLogo.PNG'
+
+
 
 export default function Splash() {
     const [text, setText] = useState("Unpuzzle the pieces");
@@ -12,16 +15,25 @@ export default function Splash() {
 
     const set = () => {
         setText(`The purpose of this ice-breaker is to raise awareness to the challenges dyslexic people face.`);
-        setSolved(!solved)
+        setTimeout(()=>{
+            setSolved(!solved)
+        }, 2500)
     };
 
     return (
         <div className='puzzle'>
-
+            <Link to='/home'>
+            <section className='topBar'>
+                <img src={fav} alt='logo' className='logoImg' />
+                <h1>ACCESS & LEARN</h1>
+            </section>
+            </Link>
+            <br />
+            <h3>{text}</h3>
             {
-                solved ? <img src={solvedPic} alt='solved' /> : <JigsawPuzzle
+                solved ? <img src={solvedPic} alt='solved' className='jigsaw-puzzle' /> : <JigsawPuzzle
                     imageSrc={scrambled}
-                    rows={3}
+                    rows={2}
                     columns={2}
                     onSolved={set}
                     className="jigsaw-puzzle"
@@ -29,9 +41,8 @@ export default function Splash() {
             }
             <br />
             {
-                solved ? <button><Link to='/home'>ENTER</Link></button> : null
+                solved ? <button><Link to='/home' className='buttonLink'>ENTER</Link></button> : null
             }
-
         </div>
     )
 }
