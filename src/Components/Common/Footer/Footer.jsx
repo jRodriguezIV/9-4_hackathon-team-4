26200ef2543da894f48d4e00220f116958efb3f9
jRodriguezIV <j.a.rodrigueziv@gmail.com>
2023-07-23 16:10:32 -0400

    added Footer functionality and styling

--- src/Components/Common/Footer/Footer.jsx
+++ src/Components/Common/Footer/Footer.jsx
@@ -1,11 +1,16 @@
 import './Footer.css'
+import { Link } from 'react-router-dom'
+import logo from '../NavBar/Assets/logo.PNG'
 
 export default function Footer() {
 
 
   return (
     <div className="Footer">
-      Footer
+      <div className='footerText'>
+        <p className='footerContact'><Link to='/contact'>Contact Us</Link></p>&nbsp;<img className='footerImg' src={logo} alt="Logo" />&nbsp;
+        <p className='footerAbout'>Designed by <Link to='/about'>The Power Squad</Link></p>
+      </div>
     </div>
   )
 }
