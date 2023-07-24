f8549a28f4fd7d109102137e6cbab2db9b2b6004
pjungjs <pjungjs@gmail.com>
2023-07-24 00:09:34 -0400

    updated play and pause button on CentersTable.jsx

--- src/App.js
+++ src/App.js
@@ -3,31 +3,29 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useState } from 'react';
 
 import Navbar from './Components/Common/NavBar/NavBar';
-import Footer from './Components/Common/Footer/Footer.jsx';
-import NotFound from './Pages/NotFound/NotFound.jsx';
-
+import Splash from './Pages/SplashPage/SplashPage';
 import LandingPage from './Pages/LandingPage/LandingPage.jsx';
 import Centers from './Components/Centers/Centers.jsx';
 import ContactUs from './Pages/ContactUs/ContactUs';
 import About from './Components/About';
 import TestComponent from './Components/TestComponent/TestComponent';
-import Splash from './Pages/SplashPage/SplashPage';
+import NotFound from './Pages/NotFound/NotFound.jsx';
+import Footer from './Components/Common/Footer/Footer.jsx';
 
 
 function App() {
   const [theme, setTheme] = useState('light')
 
-  const toggleTheme = () =>{
-    if(theme === 'light'){
+  const toggleTheme = () => {
+    if (theme === 'light'){
       setTheme('dark')
-    } else{
+    } else {
       setTheme('light')
     }
   }
   
   return (
     <div className={`App ${theme}`}>
-
       <Router>
         <Navbar toggleTheme={toggleTheme} theme={theme}/>
         <main>
