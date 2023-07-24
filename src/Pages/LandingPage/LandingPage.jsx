e6b4f6b8260fda3053914166a0ea91d7c3ca60e6
jRodriguezIV <j.a.rodrigueziv@gmail.com>
2023-07-24 00:39:47 -0400

    added pause functionality to speech synthesis in LandingPage

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -1,27 +1,44 @@
 import CarouselComp from "../../Components/Carousel/CarouselComp"
-import { HiPlay, HiStop } from 'react-icons/hi2'
+import { HiPlayPause, HiStop } from 'react-icons/hi2'
 import "./LandingPage.css"
 
-let speech = new SpeechSynthesisUtterance();
 
 function LandingPage() {
 
+  let speech = new SpeechSynthesisUtterance();
+  let synth = window.speechSynthesis;
+
  let textToSpeech1 = () => {
+  if(!synth.speaking && !synth.paused) {
+
+  
     speech.text = "DISCOVER NEARBY LITERACY PROGRAMS AND SUPPORT."
     speech.rate = 0.75
-    window.speechSynthesis.speak(speech)
+    synth.speak(speech)
+  } else {
+    synth.paused ? synth.resume() : synth.pause();
+  }
   }
 
   let textToSpeech2 = () => {
+    if(!synth.speaking && !synth.paused) {
     speech.text = "OUR USER-FRIENDLY APP CONNECTS YOU WITH LOCATIONS FOR EASY ACCESS TO RESOURCES."
     speech.rate = 0.75
     window.speechSynthesis.speak(speech)
+  } else {
+    synth.paused ? synth.resume() : synth.pause();
+  }
+
   }
 
   let textToSpeech3 = () => {
+    if(!synth.speaking && !synth.paused){
     speech.text = "EMPOWERING COMMUNITIES THROUGH LITERACY."
     speech.rate = 0.75
     window.speechSynthesis.speak(speech)
+    }else {
+      synth.paused ? synth.resume() : synth.pause();
+    }
   }
 
 
@@ -36,11 +53,11 @@ function LandingPage() {
       <div className='textCard'>
         <h3>
         DISCOVER NEARBY LITERACY PROGRAMS AND SUPPORT.
-        &nbsp;<button className='textCardPlayButton' onClick={() => textToSpeech1()}><HiPlay /></button>&nbsp;<button className='textCardPauseButton' onClick={() => speechPause()}><HiStop /></button>
+        &nbsp;<button className='textCardPlayButton' onClick={() => textToSpeech1()}><HiPlayPause /></button>&nbsp;<button className='textCardPauseButton' onClick={() => speechPause()}><HiStop /></button>
         <br/><br/> OUR USER-FRIENDLY APP CONNECTS YOU WITH LOCATIONS FOR EASY ACCESS TO RESOURCES.
-        &nbsp;<button className='textCardPlayButton' onClick={() => textToSpeech2()}><HiPlay /></button>&nbsp;<button className='textCardPauseButton' onClick={() => speechPause()}><HiStop /></button>
+        &nbsp;<button className='textCardPlayButton' onClick={() => textToSpeech2()}><HiPlayPause /></button>&nbsp;<button className='textCardPauseButton' onClick={() => speechPause()}><HiStop /></button>
         <br/><br/>EMPOWERING COMMUNITIES THROUGH LITERACY.
-        &nbsp;<button className='textCardPlayButton' onClick={() => textToSpeech3()}><HiPlay /></button>&nbsp;<button className='textCardPauseButton' onClick={() => speechPause()}><HiStop /></button>
+        &nbsp;<button className='textCardPlayButton' onClick={() => textToSpeech3()}><HiPlayPause /></button>&nbsp;<button className='textCardPauseButton' onClick={() => speechPause()}><HiStop /></button>
       
         </h3>
       </div>
